9a12f77f946d56dfbb9df69c8b3d978ece68012d
Dexter <dexter.lente18@gmail.com>
2024-05-22 11:09:33 +0800

    added new routes

---
+++ src/app/api/novels/random/single/[id]/route.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from "axios";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<Response> {
+  const id = params.id;
+
+  let url = `${process.env.API_URL}/get-single-novel-random/${id}`;
+
+  const response = await axios.get(url);
+
+  const data = await response.data;
+
+  return Response.json(data);
+}
