899e3748c14d7dec6dcfd984746a2e229e4f9fa0
Dexter <dexter.lente18@gmail.com>
2024-05-24 15:06:52 +0800

    adde4d search and nav click

--- src/app/_nav/NavBar.tsx
+++ src/app/_nav/NavBar.tsx
@@ -1,28 +1,22 @@
 "use client";
 import React from "react";
-import { Input, InputProps } from "@/components/ui/input";
-import { FaMagnifyingGlass } from "react-icons/fa6";
-
-const SearchInput = (
-  props: React.JSX.IntrinsicAttributes &
-    InputProps &
-    React.RefAttributes<HTMLInputElement>
-) => {
-  return (
-    <div className="relative">
-      <Input {...props} className="pl-10 pr-3 rounded-full" />
-      <FaMagnifyingGlass className="absolute inset-y-0 left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-    </div>
-  );
-};
+import SearchComponent from "./searchcomponent";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
+  const { push } = useRouter();
+
   return (
     <div className="bg-[#131415] h-[64px]">
       <div className="flex justify-between items-center h-full px-4">
-        <div className="text-white font-bold text-xl">Master Novels</div>
-        <div className="pr-3 w-[200px]">
-          <SearchInput placeholder="Search..." />
+        <div
+          className="text-white font-bold text-xl cursor-pointer"
+          onClick={() => push("/")}
+        >
+          Master Novels
+        </div>
+        <div>
+          <SearchComponent />
         </div>
       </div>
     </div>
---
+++ src/app/_nav/searchcomponent.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
+import { useRouter } from "next/navigation";
+
+function SearchComponent() {
+  const [title, setTitle] = useState("");
+  const [searching, setSearching] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (title.trim() !== "") {
+      router.replace(`/search?title=${title}`);
+      setSearching((e) => !e);
+    }
+  };
+
+  const handleClick = () => {
+    setSearching((e) => !e);
+    setTitle("");
+  };
+
+  return (
+    <>
+      <div>
+        <div className="flex">
+          {searching && (
+            <div className="flex mt-[3px]">
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    className="border-2 rounded-full pb-1 p-[2px] focus:outline-none focus:border-transparent"
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder=" Search here....."
+                    style={{
+                      padding: "2px",
+                      paddingLeft: "10px",
+                      borderColor: "none !important",
+                    }}
+                  />
+                  <button
+                    className="text-[12px] rounded-full bg-[#e3e3e3] ml-1 mr-2 text-black p-[7px]"
+                    type="submit"
+                  >
+                    GO
+                  </button>
+                </form>
+              </div>
+              <div className="flex items-center justify-center">
+                <IoSearch
+                  onClick={handleClick}
+                  size={25}
+                  style={{ cursor: "pointer" }}
+                  color="white"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+        {!searching && (
+          <div className="mt-[3px]">
+            <IoSearch
+              onClick={handleClick}
+              size={25}
+              style={{ cursor: "pointer" }}
+              color="white"
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default SearchComponent;
