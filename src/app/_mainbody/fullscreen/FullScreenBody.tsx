47bf03a5ba16e7bc589500eb492b8bc21a141e4a
dexter <dexter.lente18@gmail.com>
2025-01-17 05:45:52 +0800

    added back slider

--- src/app/_mainbody/fullscreen/FullScreenBody.tsx
+++ src/app/_mainbody/fullscreen/FullScreenBody.tsx
@@ -12,7 +12,7 @@ const FullScreenBody = () => {
           <p className="text-2xl">First, Random Series </p>
           <p className="mb-4">Let’s read some random stories by genre! </p>
         </div>
-        {/* <Slider /> */}
+        <Slider />
         <div className="mt-6">
           <p className="text-2xl text-white mb-3">Popular Genres</p>
           <Randomgenre />
