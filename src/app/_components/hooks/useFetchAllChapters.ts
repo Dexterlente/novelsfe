be16534d1df34ef19bf6ee21b7d6cdf833bf6008
dexter <dexter.lente18@gmail.com>
2025-01-22 18:27:32 +0800

    update

--- src/app/_components/hooks/useFetchAllChapters.ts
+++ src/app/_components/hooks/useFetchAllChapters.ts
@@ -1,9 +1,40 @@
-import useSWR from "swr";
-import { fetcher } from "../utils/fetcher";
+import { useState, useEffect } from 'react';
+import decodeProtobuf from './decodeProtoBuf';
+import { chapters } from '@/app/_proto/chapterlist';
 
-export const useFetchAllChapters = (id: string) => {
-  const url = `/api/novels/chapters/all/${id}`;
-  const { data, isLoading } = useSWR(url, fetcher);
+export const useFetchAllChapters = (id: string, page:any) => {
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-  return { data, isLoading };
+  useEffect(() => {
+  let url = `/api/novels/chapters/all/${id}`;
+
+  if (page !== undefined || page !== null || page !== "") {
+    url += `&page=${page}`;
+  }
+// TODO ADD REVERSE PARAMS
+  const fetchProtobufData = async () => {
+      try {
+        const response = await fetch(url, {
+          headers: { 'Accept': 'application/x-protobuf' },
+        });
+
+        const base64String = await response.text();
+
+        const decodedData = decodeProtobuf(base64String, chapters.ChaptersList.deserializeBinary);
+        
+        setData(decodedData);
+      } catch (err: any) {
+        setError(err?.message || 'Error fetching or decoding data');
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProtobufData();
+  }, []);
+  return { data, isLoading, error };
+
 };
