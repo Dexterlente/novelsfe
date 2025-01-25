8210a2f23d9e72a3c025788049cd1c7ace284278
dexter <dexter.lente18@gmail.com>
2025-01-26 04:27:47 +0800

    commented context

--- src/app/_mainbody/lgscreen/LgScreenBody.tsx
+++ src/app/_mainbody/lgscreen/LgScreenBody.tsx
@@ -6,22 +6,23 @@ import LatestNovels from "../part/latestNovels";
 
 const LgScreenBody = () => {
   return (
-    <LgScreenAdhoc>
-      <div className="min-h-screen mt-4">
-        <div className="text-white">
-          <p className="text-2xl">First, Random Series </p>
-          <p className="mb-4">Let’s read some random stories by genre! </p>
-        </div>
-        <Slider />
-        <div className="mt-6">
-          <p className="text-2xl text-white mb-3">Popular Genres</p>
-          <Randomgenre />
-        </div>
-        <div className="mt-10 mb-8">
-          <LatestNovels />
-        </div>
-      </div>
-    </LgScreenAdhoc>
+    // <LgScreenAdhoc>
+    //   <div className="min-h-screen mt-4">
+    //     <div className="text-white">
+    //       <p className="text-2xl">First, Random Series </p>
+    //       <p className="mb-4">Let’s read some random stories by genre! </p>
+    //     </div>
+    //     <Slider />
+    //     <div className="mt-6">
+    //       <p className="text-2xl text-white mb-3">Popular Genres</p>
+    //       <Randomgenre />
+    //     </div>
+    //     <div className="mt-10 mb-8">
+    //       <LatestNovels />
+    //     </div>
+    //   </div>
+    // </LgScreenAdhoc>
+    <></>
   );
 };
 
