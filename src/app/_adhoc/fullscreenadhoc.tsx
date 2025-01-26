77bdb187b52b2d67173915e0799aeb27d92af0a5
dexter <dexter.lente18@gmail.com>
2025-01-26 21:07:29 +0800

    fix layout

--- src/app/_adhoc/fullscreenadhoc.tsx
+++ src/app/_adhoc/fullscreenadhoc.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const FullScreenAdhoc = ({ children }: Props) => {
   return (
-    <div>
-      <div className="grid grid-cols-[1fr,3fr,1fr]">
+    <div className="block">
+      <div className="grid grid-cols-1 mx-3 sm:grid-cols-[1fr,3fr,1fr]">
         <div></div>
         <div>{children}</div>
         <div></div>
