2523f29c7e9661f280d1b1db9aa2aeb933317363
dexter <dexter.lente18@gmail.com>
2025-01-30 17:06:23 +0800

    update route

---
+++ src/app/api/novels/genre/all/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest,) {
+
+    let url =  `${process.env.API_URL}/novels`
+
+    const page = req.nextUrl.searchParams.get("page");
+
+    if (page !== null) {
+      url += `?page=${page}&timestamp=${Date.now()}`;
+    }
+  
+    console.log('Fetching from URL:', url); // Debugging log
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Accept: "application/x-protobuf",
+          },
+        });
+  
+        return response;
+  
+      } catch (error) {
+        console.error("Error fetching protobuf data:", error);
+        return new Response("Error", { status: 500 });
+      }
+    }
+  
---
+++ src/app/api/novels/genre/filter/[id]/route.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from "axios";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<Response> {
+  const id = params.id;
+
+  let url = `${process.env.API_URL}/novels/single/?genre=${id}`;
+
+  const page = req.nextUrl.searchParams.get("page");
+
+  if (page !== null) {
+    url += `?page=${page}&timestamp=${Date.now()}`;
+  }
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Accept: "application/x-protobuf",
+      },
+    });
+
+    return response;
+
+  } catch (error) {
+    console.error("Error fetching protobuf data:", error);
+    return new Response("Error", { status: 500 });
+  }
+}
